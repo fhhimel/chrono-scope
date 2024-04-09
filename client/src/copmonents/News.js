6d5523ebf933e0ca7c1ff1b98d1eaf69f68a7072
fhhimel <fhhimel18@gmail.com>
2024-04-09 23:53:36 +0600

    updates

--- client/src/copmonents/News.js
+++ client/src/copmonents/News.js
@@ -10,6 +10,7 @@ const News = () => {
       `https://newsdata.io/api/1/news?apikey=${process.env.REACT_APP_NEWS_API_KEY}&country=bd`
     );
     setArticles(response.data.results);
+    console.log(response.data.results);
   };
   useEffect(() => {
     fetchArticles();
@@ -18,11 +19,12 @@ const News = () => {
     <>
       {articles.map((element) => {
         return (
-          <div className="col-sm-4">
+          <div className="col-lg-4">
             <NewsItem
               article_id={element.article_id}
               title={element.title}
               description={element.description.slice(0, 200) + "..."}
+              //description={element.description}
               image={element.image_url}
               link={element.link}
             />
